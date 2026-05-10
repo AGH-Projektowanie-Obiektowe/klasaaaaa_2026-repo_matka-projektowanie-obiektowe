11b911843377e3cdce351f77ca3520b58950def1
ArkadiuszMin <arernonokakaka2@gmail.com>
2026-05-10 18:27:40 +0200

    refactor

--- Lab5/src/main/java/LilKlaski/UserProfile.java
+++ Lab5/src/main/java/LilKlaski/UserProfile.java
@@ -4,6 +4,11 @@ public class UserProfile {
     private boolean isStudent;
     private int loyaltyPoints;
 
+    public UserProfile(boolean isStudent, int loyaltyPoints){
+        this.isStudent = isStudent;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
     public boolean isStudent() { return isStudent; }
     public void setStudent(boolean student) { isStudent = student; }
     public int getLoyaltyPoints() { return loyaltyPoints; }
